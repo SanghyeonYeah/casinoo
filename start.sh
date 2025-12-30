fca7ae82aad63a10e25c266154e8988412fa6577
SanghyeonYeah <jeff090413@gmail.com>
2025-12-30 12:57:06 +0900

    start

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+
+# frontend와 backend 동시에 실행 가능
+# backend 실행
 cd backend
+if ! command -v node &> /dev/null
+then
+    echo "Node.js가 설치되어 있지 않습니다. 설치해주세요."
+    exit 1
+fi
+
+# 백엔드 서버 실행
 node server.js
